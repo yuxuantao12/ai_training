da6f56f035edba23512def06061d2937ea5b88f3
Yuxuan Tao <144768397+yuxuantao12@users.noreply.github.com>
2025-08-06 11:43:46 +0900

    Create index.ts

---
+++ day1/04-mcp-server/src/index.ts
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import { FastMCP } from "fastmcp";
+import { z } from "zod";
+
+dotenv.config();
+
+const mcpServer = new FastMCP({
+  name: "MyMCPServer",
+  version: "1.0.0",
+});
+
+mcpServer.addTool({
+  name: "add",
+  description: "Add two numbers",
+  parameters: z.object({
+    a: z.number(),
+    b: z.number(),
+  }),
+  execute: async (args) => {
+    console.log("Executing add tool with args:", args);
+    return String(args.a + args.b);
+  },
+});
+
+async function main() {
+  mcpServer.start({
+    transportType: "httpStream",
+    httpStream: {
+      port: 8000,
+    },
+  });
+}
+
+main();
